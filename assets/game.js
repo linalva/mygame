16679d3b2e55d19ad0633d65127baca6e422f065
linalva <aylin.alvarezh@gmail.com>
2023-03-30 15:48:01 -0600

    Commit

--- assets/game.js
+++ assets/game.js
@@ -50,7 +50,7 @@ const generateGame = () => {
         throw new Error("The dimension of the board must be an even number.")
     }
 
-    const emojis = ['🥔', '🍒', '🥑', '🌽', '🥕', '🍇', '🍉', '🍌', '🥭', '🍍']
+    const emojis = ['🍕', '🍔', '🍟', '🌭', '🍿', '🥓', '🥗', '🥨', '🍛', '🍜','🍧', '🥐', '🥞', '🍰', '🍗', '🎂', '🥩', '🥟', '🍥', '🥙']
     const picks = pickRandom(emojis, (dimensions * dimensions) / 2) 
     const items = shuffle([...picks, ...picks])
     const cards = `
